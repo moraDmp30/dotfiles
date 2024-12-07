937fd209c8ac58926950617501d82f15d0eddcfa
Dani Mora <mora@square1.io>
2024-12-07 13:33:23 +0100

    phpcs changes

--- shell/.php-cs-fixer-config/.php-cs-fixer-8-x.php
+++ shell/.php-cs-fixer-config/.php-cs-fixer-8-x.php
@@ -62,7 +62,7 @@ return (new PhpCsFixer\Config())
         'new_with_braces' => true,
         'no_blank_lines_after_class_opening' => true,
         'no_blank_lines_after_phpdoc' => true,
-        'no_blank_lines_before_namespace' => false,
+        'blank_lines_before_namespace' => true,
         'no_empty_comment' => true,
         'no_empty_phpdoc' => true,
         'no_empty_statement' => true,
@@ -198,7 +198,6 @@ return (new PhpCsFixer\Config())
         // 'self_accessor' => true,
         'short_scalar_cast' => true,
         'single_blank_line_at_eof' => true,
-        'single_blank_line_before_namespace' => true,
         'single_class_element_per_statement' => [
             'elements' => [
                 'const',
